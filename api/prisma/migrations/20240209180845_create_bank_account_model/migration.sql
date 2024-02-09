7d1b79bb5be2e74fc15035aa0475da8772f88454
Caleb Lima <lima.cardoso.caleb@gmail.com>
2024-02-09 16:01:06 -0300

    feat: create tables schema in prisma

---
+++ api/prisma/migrations/20240209180845_create_bank_account_model/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - The primary key for the `users` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - Changed the type of `id` on the `users` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- CreateEnum
+CREATE TYPE "BankAccountType" AS ENUM ('CHECKING', 'INVESTMENT', 'CASH');
+
+-- AlterTable
+ALTER TABLE "users" DROP CONSTRAINT "users_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" UUID NOT NULL,
+ADD CONSTRAINT "users_pkey" PRIMARY KEY ("id");
+
+-- CreateTable
+CREATE TABLE "bank_accounts" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "initial_balance" DOUBLE PRECISION NOT NULL,
+    "type" "BankAccountType" NOT NULL,
+    "color" TEXT NOT NULL,
+
+    CONSTRAINT "bank_accounts_pkey" PRIMARY KEY ("id")
+);
